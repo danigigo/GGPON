9c6ea0965c1a40eb4e533ef606d11b71f91f9253
danigigo <81236304+danigigo@users.noreply.github.com>
2023-10-22 15:49:30 -0500

    Update 1_AGROSAT.py

--- 1_AGROSAT.py
+++ 1_AGROSAT.py
@@ -22,7 +22,7 @@ st.markdown("""<p style='text-align: justify;'>En el siempre cambiante panorama 
 # :red[""]
 
 
-image = Image.open('agro.JPG')
+image = Image.open('agro.jpg')
 
 st.image(image)
 
