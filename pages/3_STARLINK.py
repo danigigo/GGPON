ac9d8a08bb4e85e0daf152e9e5bb8783a427b3d8
Gamesacar <sajoht14@hotmail.com>
2023-10-24 21:48:36 -0500

    a

--- pages/3_STARLINK.py
+++ pages/3_STARLINK.py
@@ -16,39 +16,32 @@ st.sidebar.success("DANIEL SANTIAGO CAMACHO CALVO \n \n SANTIAGO JOSE HERRERA TO
 st.markdown("""<p style='text-align: justify;'>Durante el desarrolo de este proyecto se usaron diversos equipos basicos como ARDUINO UNO y herramientas de software, pero aca vemo los equipos mas relevantes que se usaron.</p>""",unsafe_allow_html=True)
 
 # OLT (Optical Line Terminal)
-st.markdown('<h3 style="color:green">ESTACION METEOROOGICA y weather shield SPARKFUN</h3>', unsafe_allow_html=True)
+st.markdown('<h3 style="color:green">CONECTIVIDAD DONDE MENOS SE ESPERA</h3>', unsafe_allow_html=True)
 
-st.markdown("""<p style='text-align: justify;'><br>El Weather Shield de SparkFun es un accesorio diseñado para placas Arduino que proporciona una solución compacta para medir y
-         monitorear diversas condiciones meteorológicas. Este shield está equipado con sensores que permiten medir la temperatura, 
-         la humedad relativa, la presión atmosférica y la velocidad y dirección del viento. Estos datos son útiles para crear estaciones 
-         meteorológicas personales, proyectos de IoT relacionados con el clima, o para monitorear y registrar las condiciones ambientales 
-         en tiempo real.El Weather Shield incluye sensores como el BMP180 para la presión atmosférica, el HTU21D para la temperatura y la 
-         humedad, y un sensor de anemómetro y veleta para medir la velocidad y dirección del viento. La información capturada por estos 
-         sensores se puede utilizar para realizar pronósticos del tiempo, estudios climáticos y más. Es una herramienta versátil para proyectos
-         relacionados con la meteorología y el monitoreo ambiental.</br><br></br></p>""",unsafe_allow_html=True)
+st.markdown("""<p style='text-align: justify;'><br>Hacer streaming, las videollamadas, juegos en línea, el trabajo 
+            remoto y más ahora son posibles incluso en los lugares más remotos gracias al sistema de Internet más 
+            avanzado del mundo.</br><br></br></p>""",unsafe_allow_html=True)
             
-
-         
-image3 = Image.open('esta.jpg')
-st.image(image3)
-image4 = Image.open('siono.jpg')
-st.image(image4)
-
-st.markdown('<h3 style="color:green">ANTENA DE INTERNET SATELITAL Y MODEN DE STARLINK </h3>', unsafe_allow_html=True)
-
-st.markdown("""<p style='text-align: justify;'><br>El internet satelital de Starlink utiliza una antena parabólica de 23 pulgadas para recibir
-            señales de una constelación de satélites de órbita terrestre baja. La antena está conectada a un módem que convierte las señales satelitales en una conexión
-         Wi-Fi.La antena parabólica es el componente más importante del sistema Starlink. Está diseñada para apuntar al cielo y recibir
-         señales de los satélites. La antena está equipada con un motor que la mantiene apuntando en la dirección correcta, incluso si 
-         el cielo está nublado o hay obstáculos en el camino.El módem es el componente que convierte las señales satelitales en una conexión
-         Wi-Fi. El módem está conectado a la antena parabólica y a un router Wi-Fi. El router Wi-Fi distribuye la conexión a otros dispositivos
-         en la casa.Starlink ofrece velocidades de descarga de hasta 200 Mbps y velocidades de subida de hasta 10 Mbps. El servicio está
-         disponible en zonas rurales y urbanas de todo el mundo.En resumen, el internet satelital de Starlink utiliza una antena parabólica y 
-         un módem para proporcionar una conexión a Internet de alta velocidad a zonas rurales y urbanas de todo el mundo.</br><br></br></p>""",unsafe_allow_html=True)
-         
-
-image9 = Image.open('star.jpg')
-st.image(image9)
 
 video_url = "https://www.starlink.com/assets/videos/SpaceX%20Starlink%20Explainer%20Video%201080p%20desktop.mp4"
 st.video(video_url)
+        
+
+image9 = Image.open('mapa.png')
+st.image(image9)
+
+st.markdown("""<p style='text-align: justify;'><br>Los soportes diseñados para instalarse permanentemente en un techo,
+            poste o pared para evitar obstrucciones, estarán disponibles en la tienda de Starlink una vez que su Starlink
+            esté listo para enviarse. También hay kits de enrutamiento de cables, longitudes de cable adicionales, nodos
+            en malla y adaptadores Ethernet disponibles.</br><br></br></p>""",unsafe_allow_html=True)
+# Colocar las imágenes en una lista
+images = ['star.jpg',
+          'router.jpg',
+          'adaptador.jpg',
+          'soporte.jpg']
+
+# Mostrar las imágenes en una disposición en mosaico
+image_index = st.slider('Mueve la barra para ver mas enemigos', 0, len(images)-1)
+
+# Mostrar la imagen seleccionada
+st.image(images[image_index], use_column_width=True)
